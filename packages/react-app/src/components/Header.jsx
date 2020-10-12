c39d5129982aaae00f338ba9ea6b5e8647b696a6
funveco <funveco@gmail.com>
2020-10-12 11:28:30 -0430

    First UPLOAD

---
+++ packages/react-app/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { PageHeader } from "antd";
+
+export default function Header() {
+  return (
+    <a href="" target="_blank" rel="noopener noreferrer">
+      <PageHeader
+        title="BIENES RAICES - TRANSFERENCIA DE PROPIETARIO"
+        subTitle="ADDRESS_REGISTER_GOVERNOR_REAL_STATE_PROPERTY_TRANSFER"
+        style={{ cursor: "pointer" }}
+      />
+    </a>
+  );
+}
